952519631213587c3c4edbaff420b055b279dee4
Yikal <renespa@web.de>
2019-10-04 11:04:11 +0200

    Added regions to output CSV file (from StatesToRegions.csv)

---
+++ Region.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper.Configuration;
+
+namespace scrapysharp_dt2020
+{
+	public class Region
+	{
+		public string state;
+		public string region;
+
+		public Region () { }
+		public Region (string state, string region)
+		{
+			this.region = region;
+			this.state = state;
+		}
+	}
+
+	public sealed class RegionCsvMap : ClassMap<Region>
+	{
+		public RegionCsvMap ()
+		{
+			Map(m => m.state).Name("State");
+			Map(m => m.region).Name("Region");
+		}
+	}
+}
